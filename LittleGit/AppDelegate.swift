a376cbfa2fe229db3082a3475ab6fe1460abd6e7
Uli Kusterer <ulivc@zathras.de>
2015-12-06 02:33:19 +0100

    Initial Commit

---
+++ LittleGit/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  LittleGit
+//
+//  Created by Uli Kusterer on 06/12/15.
+//  Copyright © 2015 Uli Kusterer. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+	@IBOutlet weak var window: NSWindow!
+
+
+	func applicationDidFinishLaunching(aNotification: NSNotification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(aNotification: NSNotification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
